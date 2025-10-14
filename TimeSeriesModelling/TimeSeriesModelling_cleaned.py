58d6e8d335b8d3b103c76838d3991d6278210c8a
Ivan Kukuljan <ivan.kukuljan.ju@gmail.com>
2025-10-14 23:17:26 +0200

    Clean plots

--- TimeSeriesModelling/TimeSeriesModelling_cleaned.py
+++ TimeSeriesModelling/TimeSeriesModelling_cleaned.py
@@ -5,6 +5,7 @@ import re
 import os
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import matplotlib.cm as cm
 from matplotlib.ticker import MaxNLocator
 from matplotlib.lines import Line2D
 from datetime import datetime, date
@@ -22,34 +23,26 @@ except NameError:
     # Fallback for notebooks or interactive mode
     BASE_DIR = os.getcwd()
 
-eurostat_datasets = {'Unemployment Rate' :
+eurostat_datasets = {'Gross domestic product (GDP) (2015 = 100)' :
+{'dataset_code' : 'nama_10_gdp',
+'frequency' : 'a',
+'url' : 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/nama_10_gdp/1.0/*.*.*.*?c[freq]=A&c[unit]=CLV_I15&c[na_item]=B1GQ&c[geo]=SI&c[TIME_PERIOD]=2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006&compress=false&format=csvdata&formatVersion=2.0&lang=en&labels=name',
+'unit_column_name' : 'Unit of measure',
+'fill_missing' : True},
+
+'Gross domestic product (GDP) (percentage change on previous)' :    
+{'dataset_code' : 'nama_10_gdp',
+'frequency' : 'a',
+'url' : 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/nama_10_gdp/1.0/*.*.*.*?c[freq]=A&c[unit]=CLV_PCH_PRE&c[na_item]=B1GQ&c[geo]=SI&c[TIME_PERIOD]=2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006&compress=false&format=csvdata&formatVersion=2.0&lang=en&labels=name',
+'unit_column_name' : 'Unit of measure',
+'fill_missing' : True},
+
+ 'Unemployment Rate' :
  {'dataset_code' : 'une_rt_q',
  'frequency' : 'q',
  'url' : 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/une_rt_q/1.0/*.*.*.*.*.*?c[freq]=Q&c[s_adj]=SA&c[age]=Y20-64&c[unit]=PC_ACT&c[sex]=T&c[geo]=SI&c[TIME_PERIOD]=ge:2006-Q1+le:2023-Q4&compress=false&format=csvdata&formatVersion=2.0&lang=en&labels=name',
  'unit_column_name' : 'Unit of measure',
  'fill_missing' : False},
-                     
- 'Gross domestic product (GDP) (2015 = 100)' :
- {'dataset_code' : 'nama_10_gdp',
- 'frequency' : 'a',
- 'url' : 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/nama_10_gdp/1.0/*.*.*.*?c[freq]=A&c[unit]=CLV_I15&c[na_item]=B1GQ&c[geo]=SI&c[TIME_PERIOD]=2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006&compress=false&format=csvdata&formatVersion=2.0&lang=en&labels=name',
- 'unit_column_name' : 'Unit of measure',
- 'fill_missing' : True},
-
- 'Gross domestic product (GDP) (percentage change on previous)' :    
- {'dataset_code' : 'nama_10_gdp',
- 'frequency' : 'a',
- 'url' : 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/nama_10_gdp/1.0/*.*.*.*?c[freq]=A&c[unit]=CLV_PCH_PRE&c[na_item]=B1GQ&c[geo]=SI&c[TIME_PERIOD]=2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006&compress=false&format=csvdata&formatVersion=2.0&lang=en&labels=name',
- 'unit_column_name' : 'Unit of measure',
- 'fill_missing' : True},
-
- 'Money Market Interest Rate' : 
- {'dataset_code' : 'irt_st_m',
- 'frequency' : 'm',
- 'url' : 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/irt_st_m/1.0/*.*.*?c[freq]=M&c[int_rt]=IRT_M12&c[geo]=EA&c[TIME_PERIOD]=2023-12,2023-11,2023-10,2023-09,2023-08,2023-07,2023-06,2023-05,2023-04,2023-03,2023-02,2023-01,2022-12,2022-11,2022-10,2022-09,2022-08,2022-07,2022-06,2022-05,2022-04,2022-03,2022-02,2022-01,2021-12,2021-11,2021-10,2021-09,2021-08,2021-07,2021-06,2021-05,2021-04,2021-03,2021-02,2021-01,2020-12,2020-11,2020-10,2020-09,2020-08,2020-07,2020-06,2020-05,2020-04,2020-03,2020-02,2020-01,2019-12,2019-11,2019-10,2019-09,2019-08,2019-07,2019-06,2019-05,2019-04,2019-03,2019-02,2019-01,2018-12,2018-11,2018-10,2018-09,2018-08,2018-07,2018-06,2018-05,2018-04,2018-03,2018-02,2018-01,2017-12,2017-11,2017-10,2017-09,2017-08,2017-07,2017-06,2017-05,2017-04,2017-03,2017-02,2017-01,2016-12,2016-11,2016-10,2016-09,2016-08,2016-07,2016-06,2016-05,2016-04,2016-03,2016-02,2016-01,2015-12,2015-11,2015-10,2015-09,2015-08,2015-07,2015-06,2015-05,2015-04,2015-03,2015-02,2015-01,2014-12,2014-11,2014-10,2014-09,2014-08,2014-07,2014-06,2014-05,2014-04,2014-03,2014-02,2014-01,2013-12,2013-11,2013-10,2013-09,2013-08,2013-07,2013-06,2013-05,2013-04,2013-03,2013-02,2013-01,2012-12,2012-11,2012-10,2012-09,2012-08,2012-07,2012-06,2012-05,2012-04,2012-03,2012-02,2012-01,2011-12,2011-11,2011-10,2011-09,2011-08,2011-07,2011-06,2011-05,2011-04,2011-03,2011-02,2011-01,2010-12,2010-11,2010-10,2010-09,2010-08,2010-07,2010-06,2010-05,2010-04,2010-03,2010-02,2010-01,2009-12,2009-11,2009-10,2009-09,2009-08,2009-07,2009-06,2009-05,2009-04,2009-03,2009-02,2009-01,2008-12,2008-11,2008-10,2008-09,2008-08,2008-07,2008-06,2008-05,2008-04,2008-03,2008-02,2008-01,2007-12,2007-11,2007-10,2007-09,2007-08,2007-07,2007-06,2007-05,2007-04,2007-03,2007-02,2007-01,2006-12,2006-11,2006-10,2006-09,2006-08,2006-07,2006-06,2006-05,2006-04,2006-03,2006-02,2006-01&compress=false&format=csvdata&formatVersion=2.0&lang=en&labels=name',
- 'unit_column_name' : 'Interest rate',
- 'resample_method' : 'last',
- 'fill_missing' : True},
 
  'House price index (2015 = 100)' : 
  {'dataset_code' : 'prc_hpi_q',
@@ -66,19 +59,28 @@ eurostat_datasets = {'Unemployment Rate' :
  'resample_method' : 'mean',
  'fill_missing' : True},
 
+ 'Private sectror credit flow':
+ {'dataset_code' : 'tipspc10',
+ 'frequency' : 'a',
+ 'url' : 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/tipspc10/1.0/*.*.*.*.*.*.*?c[freq]=A&c[co_nco]=CO&c[sector]=S11_S14_S15&c[finpos]=LIAB&c[na_item]=F3_F4&c[unit]=PC_GDP&c[geo]=SI&c[TIME_PERIOD]=2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006&compress=false&format=csvdata&formatVersion=2.0&lang=en&labels=name',
+ 'unit_column_name' : 'Unit of measure',
+ 'fill_missing' : True},                 
+ 
+
+ 'Money Market Interest Rate' : 
+ {'dataset_code' : 'irt_st_m',
+ 'frequency' : 'm',
+ 'url' : 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/irt_st_m/1.0/*.*.*?c[freq]=M&c[int_rt]=IRT_M12&c[geo]=EA&c[TIME_PERIOD]=2023-12,2023-11,2023-10,2023-09,2023-08,2023-07,2023-06,2023-05,2023-04,2023-03,2023-02,2023-01,2022-12,2022-11,2022-10,2022-09,2022-08,2022-07,2022-06,2022-05,2022-04,2022-03,2022-02,2022-01,2021-12,2021-11,2021-10,2021-09,2021-08,2021-07,2021-06,2021-05,2021-04,2021-03,2021-02,2021-01,2020-12,2020-11,2020-10,2020-09,2020-08,2020-07,2020-06,2020-05,2020-04,2020-03,2020-02,2020-01,2019-12,2019-11,2019-10,2019-09,2019-08,2019-07,2019-06,2019-05,2019-04,2019-03,2019-02,2019-01,2018-12,2018-11,2018-10,2018-09,2018-08,2018-07,2018-06,2018-05,2018-04,2018-03,2018-02,2018-01,2017-12,2017-11,2017-10,2017-09,2017-08,2017-07,2017-06,2017-05,2017-04,2017-03,2017-02,2017-01,2016-12,2016-11,2016-10,2016-09,2016-08,2016-07,2016-06,2016-05,2016-04,2016-03,2016-02,2016-01,2015-12,2015-11,2015-10,2015-09,2015-08,2015-07,2015-06,2015-05,2015-04,2015-03,2015-02,2015-01,2014-12,2014-11,2014-10,2014-09,2014-08,2014-07,2014-06,2014-05,2014-04,2014-03,2014-02,2014-01,2013-12,2013-11,2013-10,2013-09,2013-08,2013-07,2013-06,2013-05,2013-04,2013-03,2013-02,2013-01,2012-12,2012-11,2012-10,2012-09,2012-08,2012-07,2012-06,2012-05,2012-04,2012-03,2012-02,2012-01,2011-12,2011-11,2011-10,2011-09,2011-08,2011-07,2011-06,2011-05,2011-04,2011-03,2011-02,2011-01,2010-12,2010-11,2010-10,2010-09,2010-08,2010-07,2010-06,2010-05,2010-04,2010-03,2010-02,2010-01,2009-12,2009-11,2009-10,2009-09,2009-08,2009-07,2009-06,2009-05,2009-04,2009-03,2009-02,2009-01,2008-12,2008-11,2008-10,2008-09,2008-08,2008-07,2008-06,2008-05,2008-04,2008-03,2008-02,2008-01,2007-12,2007-11,2007-10,2007-09,2007-08,2007-07,2007-06,2007-05,2007-04,2007-03,2007-02,2007-01,2006-12,2006-11,2006-10,2006-09,2006-08,2006-07,2006-06,2006-05,2006-04,2006-03,2006-02,2006-01&compress=false&format=csvdata&formatVersion=2.0&lang=en&labels=name',
+ 'unit_column_name' : 'Interest rate',
+ 'resample_method' : 'last',
+ 'fill_missing' : True},
+
  'Consumer confidence indicator':
  {'dataset_code' : 'ei_bsco_m',
  'frequency' : 'm',
  'url' : 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/ei_bsco_m/1.0/*.*.*.*.*?c[freq]=M&c[indic]=BS-CSMCI&c[s_adj]=SA&c[unit]=BAL&c[geo]=SI&c[TIME_PERIOD]=2023-12,2023-11,2023-10,2023-09,2023-08,2023-07,2023-06,2023-05,2023-04,2023-03,2023-02,2023-01,2022-12,2022-11,2022-10,2022-09,2022-08,2022-07,2022-06,2022-05,2022-04,2022-03,2022-02,2022-01,2021-12,2021-11,2021-10,2021-09,2021-08,2021-07,2021-06,2021-05,2021-04,2021-03,2021-02,2021-01,2020-12,2020-11,2020-10,2020-09,2020-08,2020-07,2020-06,2020-05,2020-04,2020-03,2020-02,2020-01,2019-12,2019-11,2019-10,2019-09,2019-08,2019-07,2019-06,2019-05,2019-04,2019-03,2019-02,2019-01,2018-12,2018-11,2018-10,2018-09,2018-08,2018-07,2018-06,2018-05,2018-04,2018-03,2018-02,2018-01,2017-12,2017-11,2017-10,2017-09,2017-08,2017-07,2017-06,2017-05,2017-04,2017-03,2017-02,2017-01,2016-12,2016-11,2016-10,2016-09,2016-08,2016-07,2016-06,2016-05,2016-04,2016-03,2016-02,2016-01,2015-12,2015-11,2015-10,2015-09,2015-08,2015-07,2015-06,2015-05,2015-04,2015-03,2015-02,2015-01,2014-12,2014-11,2014-10,2014-09,2014-08,2014-07,2014-06,2014-05,2014-04,2014-03,2014-02,2014-01,2013-12,2013-11,2013-10,2013-09,2013-08,2013-07,2013-06,2013-05,2013-04,2013-03,2013-02,2013-01,2012-12,2012-11,2012-10,2012-09,2012-08,2012-07,2012-06,2012-05,2012-04,2012-03,2012-02,2012-01,2011-12,2011-11,2011-10,2011-09,2011-08,2011-07,2011-06,2011-05,2011-04,2011-03,2011-02,2011-01,2010-12,2010-11,2010-10,2010-09,2010-08,2010-07,2010-06,2010-05,2010-04,2010-03,2010-02,2010-01,2009-12,2009-11,2009-10,2009-09,2009-08,2009-07,2009-06,2009-05,2009-04,2009-03,2009-02,2009-01,2008-12,2008-11,2008-10,2008-09,2008-08,2008-07,2008-06,2008-05,2008-04,2008-03,2008-02,2008-01,2007-12,2007-11,2007-10,2007-09,2007-08,2007-07,2007-06,2007-05,2007-04,2007-03,2007-02,2007-01,2006-12,2006-11,2006-10,2006-09,2006-08,2006-07,2006-06,2006-05,2006-04,2006-03,2006-02,2006-01&compress=false&format=csvdata&formatVersion=2.0&lang=en&labels=name',
  'unit_column_name' : 'Unit of measure',
  'resample_method' : 'mean',
- 'fill_missing' : True},
-
- 'Private sectror credit flow':
- {'dataset_code' : 'tipspc10',
- 'frequency' : 'a',
- 'url' : 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/3.0/data/dataflow/ESTAT/tipspc10/1.0/*.*.*.*.*.*.*?c[freq]=A&c[co_nco]=CO&c[sector]=S11_S14_S15&c[finpos]=LIAB&c[na_item]=F3_F4&c[unit]=PC_GDP&c[geo]=SI&c[TIME_PERIOD]=2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006&compress=false&format=csvdata&formatVersion=2.0&lang=en&labels=name',
- 'unit_column_name' : 'Unit of measure',
  'fill_missing' : True}
                     }
 
@@ -759,8 +761,9 @@ def plot_nlb_and_eurostat_data(
     except Exception:
         idx = data.index.to_list()
     ax.plot(idx, data['npl'], linewidth=3.5, color='#230078')
-    ax.set_title('NLB NPL', pad=8)
-    ax.set_xlabel('Date'); ax.set_ylabel('NPL')
+    ax.set_title('NLB NPL', color='#230078', pad=8, fontweight='bold')
+    # ax.set_xlabel('Date'); 
+    # ax.set_ylabel('NPL')
     if hasattr(idx, '__len__') and len(idx) > 1:
         loc, fmt = _year_locator_for_span(idx)
         ax.xaxis.set_major_locator(loc); ax.xaxis.set_major_formatter(fmt)
@@ -796,7 +799,8 @@ def plot_nlb_and_eurostat_data(
 
         ax.plot(x_vals, values_f.values, linewidth=2.2, color='black')
         ax.set_title(key, pad=8)
-        ax.set_xlabel('Time'); ax.set_ylabel(ylab)
+        # ax.set_xlabel('Time'); 
+        # ax.set_ylabel(ylab)
 
         # Less-dense integer years
         loc, fmt = _year_locator_for_span(x_vals)
@@ -875,11 +879,11 @@ def plot_npl_predictions(data_train, data_test_1, data_test_2, model):
         zorder=4
     )
     ax.set_title(
-        f"Training and Test 1 Data with Predictions (MSE: {mse_test_1:.4f}, R²: {r2_test_1:.4f})"
+        f"Training and Test 1 data with predictions (MSE: {mse_test_1:.4f}, R²: {r2_test_1:.4f})"
     )
-    ax.set_ylabel("NPL Value")
+    ax.set_ylabel("NPL")
     ax.legend()
-    ax.grid(True, linestyle='--', alpha=0.5)
+    # ax.grid(True, linestyle='--', alpha=0.5)
     ax.xaxis.set_major_locator(mdates.YearLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
 
@@ -893,6 +897,15 @@ def plot_npl_predictions(data_train, data_test_1, data_test_2, model):
         label='test_2 ground truth',
         zorder=3
     )
+    ax2.plot(
+    data_test_2.index,
+    data_test_2['npl'],
+    color='green',
+    linewidth=2,
+    alpha=0.7,
+    label='_nolegend_',  # so it doesn’t add a duplicate legend entry
+    zorder=2
+    )
     # Plot predicted values for second test split
     ax2.scatter(
         data_test_2.index,
@@ -904,14 +917,14 @@ def plot_npl_predictions(data_train, data_test_1, data_test_2, model):
         zorder=4
     )
     ax2.set_title(
-        f"Test 2 Data with Predictions (MSE: {mse_test_2:.4f}, R²: {r2_test_2:.4f})"
+        f"Test 2 data with predictions (MSE: {mse_test_2:.4f}, R²: {r2_test_2:.4f})"
     )
-    ax2.set_ylabel("NPL Value")
+    ax2.set_ylabel("NPL")
     ax2.legend()
-    ax2.grid(True, linestyle='--', alpha=0.5)
+    # ax2.grid(True, linestyle='--', alpha=0.5)
     ax2.xaxis.set_major_locator(mdates.YearLocator())
     ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
-    ax2.set_xlabel("Date")
+    ax2.set_xlabel("date")
 
     # Finalize layout and save/show the plot
     plt.tight_layout()
@@ -928,7 +941,7 @@ def plot_npl_predictions(data_train, data_test_1, data_test_2, model):
 def plot_abs_coeff_series_by_dataset(
     model: LinearRegression,
     feature_names: Iterable[str],
-    title: Optional[str] = "Absolute model coefficients by dataset/unit (log scale)",
+    title: Optional[str] = "Absolute model coefficients by dataset (log scale)",
     ax: Optional[plt.Axes] = None,
 ) -> plt.Axes:
     """
@@ -968,7 +981,8 @@ def plot_abs_coeff_series_by_dataset(
     series.sort(key=lambda s: s["max_abs"], reverse=True)
 
     if ax is None:
-        fig, ax = plt.subplots(figsize=(11, 6))
+        fig, ax = plt.subplots(figsize=(8, 6))
+           
 
     linestyles = ["-", "--", ":"]
     colors = [d.get("color") for d in plt.rcParams["axes.prop_cycle"]]
@@ -983,7 +997,7 @@ def plot_abs_coeff_series_by_dataset(
         # 1) line for abs values (legend label here, once)
         ax.plot(
             s["x"], s["y"],
-            linestyle=ls, color=color, linewidth=2, alpha=0.9, label=label,
+            linestyle=ls, color=color, linewidth=2, alpha=0.7, label=label,
         )
 
         # 2) sign markers (no legend entries)
@@ -995,24 +1009,25 @@ def plot_abs_coeff_series_by_dataset(
         if pos_x:
             ax.scatter(
                 pos_x, pos_y,
-                marker="^", color=color, edgecolor="black", linewidth=0.5,
-                s=55, alpha=0.95, label="_nolegend_",
+                marker="^", color=color, linewidth=0.5,
+                s=55, alpha=0.8, label="_nolegend_",
             )
         if neg_x:
             ax.scatter(
                 neg_x, neg_y,
-                marker="v", color=color, edgecolor="black", linewidth=0.5,
-                s=55, alpha=0.95, label="_nolegend_",
+                marker="v", color=color, linewidth=0.5,
+                s=55, alpha=0.8, label="_nolegend_",
             )
 
     # Axes styling
     ax.set_yscale("log")
-    ax.set_xlabel("Delay [in time units]")
-    ax.set_ylabel("|Coefficient| (log scale)")
+    ax.set_xlabel("delay in time units")
+    ax.set_ylabel("|coefficient| (log scale)")
     if title:
         ax.set_title(title)
 
-    ax.grid(True, which="both", linewidth=0.5, alpha=0.5)
+    # ax.grid(axis="y", which="both", color="gray", linewidth=0.6, alpha=0.4)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 
     # Build legend: series lines first, then black triangle sign key at the bottom
     handles, labels = ax.get_legend_handles_labels()
